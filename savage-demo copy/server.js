86b7d3c505e6e5129b989fe4457dc36e3fd23f23
Daniela Morin <dmorin@wellesley.edu>
2022-06-01 00:56:04 -0400

    Add files via upload

---
+++ savage-demo copy/server.js
@@ -0,0 +1,106 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+
+//global
+var db, collection;
+
+const url ='mongodb+srv://dmorin13:UUUbk2HSnM3qM7xj@cluster0.izgq8.mongodb.net/?retryWrites=true&w=majority'
+  //mongodb+srv: "mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites=true";
+// const dbName = "demo";
+const dbName = "savage";
+//higher order function
+app.listen(3000, () => {
+  MongoClient.connect(
+    url,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (error, client) => {
+      if (error) {
+        throw error;
+      }
+      db = client.db(dbName);
+      console.log("Connected to `" + dbName + "`!");
+    }
+  );
+});
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+//client side static files are accessible/ public?
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  db.collection("messages")
+    .find()
+    .toArray((err, result) => {
+      if (err) return console.log(err);
+      res.render("index.ejs", { messages: result });
+    });
+});
+
+app.post("/messages", (req, res) => {
+  db.collection("messages").insertOne(
+    { name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown: 0 },
+    (err, result) => {
+      if (err) return console.log(err);
+      console.log("saved to database");
+      res.redirect("/");
+    }
+  );
+});
+
+app.put("/messages", (req, res) => {
+  db.collection("messages").findOneAndUpdate(
+    { name: req.body.name, msg: req.body.msg },
+    {
+      $set: {
+        thumbUp: req.body.thumbUp + 1,
+      }
+    },
+
+    {
+      sort: { _id: -1 },
+      upsert: true,
+    },
+
+    (err, result) => {
+      if (err) return res.send(err);
+      res.send(result);
+    }
+    
+  );
+});
+app.put("/messagesDown", (req, res) => {
+  db.collection("messages").findOneAndUpdate(
+    { name: req.body.name, msg: req.body.msg },
+    {
+      $set: {
+        thumbUp:req.body.thumbUp - 1,
+      },
+    },
+
+    {
+      sort: { _id: -1 },
+      upsert: true,
+    },
+
+    (err, result) => {
+      if (err) return res.send(err);
+      res.send(result);
+    }
+    
+  );
+});
+
+
+app.delete("/messages", (req, res) => {
+  db.collection("messages").findOneAndDelete(
+    { name: req.body.name, msg: req.body.msg },
+    (err, result) => {
+      if (err) return res.send(500, err);
+      res.send("Message deleted!");
+    }
+  );
+});
